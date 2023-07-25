606dee85ceb914629796601b13bcc1e341b86701
Pandula <vg.pandula@gmail.com>
2023-07-25 11:36:01 +0530

    css changes

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -4,7 +4,7 @@ export const TodoList = () => {
   return (
     <>
       <div className="home-container">
-        <div className="home-to-do-ist">
+        <div className="home-todo-list">
           <h1 className="home-text">My Todo</h1>
           <span className="home-text1">Today Tasks</span>
           <button type="button" className="home-button button">
@@ -16,6 +16,9 @@ export const TodoList = () => {
             className="home-text-input input"
           />
         </div>
+        {/* <div className="home-todo-tasks">
+          <h3>Gihan Pandula</h3>
+        </div> */}
       </div>
     </>
   );
