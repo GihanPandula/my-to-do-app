13e08c6ecbd819f277e3fb86896b5481a3e86df8
Pandula <vg.pandula@gmail.com>
2023-07-27 11:09:55 +0530

    Sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-// import TodoHome from "./TodoHome";
-// import TodoSignUp from "./TodoSignUp";
-// import TodoLogin from "./TodoLogin";
 import React from "react";
 import HomePage from "./HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginApp from "./LoginApp";
-
+import SignUpPage from "./SignUpPage";
 
 function App() {
   return (
@@ -14,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/LoginApp" element={<LoginApp />} />
+          <Route path="/SignUpPage" element={<SignUpPage />} />
         </Routes>
       </Router>
     </div>
@@ -21,5 +19,3 @@ function App() {
 }
 
 export default App;
-
-
---
+++ src/SignUpPage.tsx
@@ -0,0 +1,66 @@
+//import { FunctionComponent, useCallback } from "react";
+import { Input } from "antd";
+//import { useNavigate } from "react-router-dom";
+import "./SignUpPage.css";
+import { FunctionComponent } from "react";
+
+
+const SignUpPage: FunctionComponent = () => {
+  //   const navigate = useNavigate();
+
+  //   const onSignUpBtnButtonClick = useCallback(() => {
+  //     navigate("/todo-app");
+  //   }, [navigate]);
+
+  return (
+    <div className="sign-up-page">
+      <div className="sign-up-parent">
+        <div className="sign-up1">Sign Up!</div>
+        <b className="get-organized-and">
+          Get organized and stay on top of your tasks with our My Todo app.
+        </b>
+      </div>
+      <div className="email-input-parent">
+        <Input
+          className="email-input"
+          type="email"
+          size="middle"
+          placeholder="Enter Email"
+          allowClear
+          bordered={false}
+        />
+        <Input
+          className="email-input"
+          type="text"
+          size="middle"
+          placeholder="Enter Name"
+          allowClear
+          bordered={false}
+        />
+
+        {/* ANT */}
+
+        <Input.Password
+          className="email-input"
+          size="middle"
+          placeholder="Enter Password"
+          allowClear
+          bordered={false}
+        />
+      </div>
+      <div className="sign-up-btn-button1">
+        <div className="sign-up2">Sign Up</div>
+      </div>
+      <b className="or-sign-up">Or Sign Up with these</b>
+      <img
+        className="icon-set"
+        alt="Google Apple Facebook"
+        src="/iconSet.svg"
+      />
+    </div>
+  );
+};
+
+export default SignUpPage;
+
+// onClick={onSignUpBtnButtonClick}
